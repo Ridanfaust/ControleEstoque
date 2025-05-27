358f862c7785a9a7a492be2168c6d17e90cd64c9
Ricardo Dantas <ricardo.dantas@esig.com.br>
2025-05-27 14:03:47 -0300

    [infra] fix: corrige inicialização de dados

--- backend/src/main/resources/db/init-data.sql
+++ backend/src/main/resources/db/init-data.sql
@@ -8,22 +8,26 @@ ON CONFLICT (id) DO UPDATE SET descricao = EXCLUDED.descricao;
 -- Insere tipos de produto
 INSERT INTO estoque.tipo_produto (id, descricao, data_cadastro, ativo)
 VALUES
-    (nextval(''estoque.tipo_produto_seq''), ''Eletrônicos'', CURRENT_DATE, true),
-    (nextval(''estoque.tipo_produto_seq''), ''Móveis e Eletrodomésticos'', CURRENT_DATE, true),
-    (nextval(''estoque.tipo_produto_seq''), ''Serviço'', CURRENT_DATE, true)
+    (1, ''Eletrônicos'', CURRENT_DATE, true),
+    (2, ''Móveis e Eletrodomésticos'', CURRENT_DATE, true),
+    (3, ''Serviço'', CURRENT_DATE, true)
 ON CONFLICT (id) DO UPDATE SET descricao = EXCLUDED.descricao, data_cadastro = EXCLUDED.data_cadastro, ativo = EXCLUDED.ativo;
 
 -- Insere tipos de movimentação
 INSERT INTO estoque.tipo_movimentacao (id, descricao, natureza_movimentacao, venda, data_cadastro, ativo)
 VALUES
     -- Tipo ENTRADA
-    (nextval(''estoque.tipo_movimentacao_seq''), ''Compra de Fornecedor'', ''ENTRADA'', false, CURRENT_DATE, true),
-    (nextval(''estoque.tipo_movimentacao_seq''), ''Compra Direto da Fábrica'', ''ENTRADA'', false, CURRENT_DATE, true),
+    (1, ''Compra de Fornecedor'', ''ENTRADA'', false, CURRENT_DATE, true),
+    (2, ''Compra Direto da Fábrica'', ''ENTRADA'', false, CURRENT_DATE, true),
 
     -- Tipos SAIDA
-    (nextval(''estoque.tipo_movimentacao_seq''), ''Venda ao Cliente'', ''SAIDA'', true, CURRENT_DATE, true),
-    (nextval(''estoque.tipo_movimentacao_seq''), ''Venda para Revendedor'', ''SAIDA'', true, CURRENT_DATE, true),
-    (nextval(''estoque.tipo_movimentacao_seq''), ''Uso Interno'', ''SAIDA'', false, CURRENT_DATE, true)
+    (3, ''Venda ao Cliente'', ''SAIDA'', true, CURRENT_DATE, true),
+    (4, ''Venda para Revendedor'', ''SAIDA'', true, CURRENT_DATE, true),
+    (5, ''Uso Interno'', ''SAIDA'', false, CURRENT_DATE, true)
 ON CONFLICT (id) DO UPDATE SET descricao = EXCLUDED.descricao, natureza_movimentacao = EXCLUDED.natureza_movimentacao, venda = EXCLUDED.venda, data_cadastro = EXCLUDED.data_cadastro, ativo = EXCLUDED.ativo;
+
+-- Atualiza as sequences para continuar após o maior ID inserido
+PERFORM setval(''estoque.tipo_produto_seq'', (SELECT MAX(id) FROM estoque.tipo_produto), true);
+PERFORM setval(''estoque.tipo_movimentacao_seq'', (SELECT MAX(id) FROM estoque.tipo_movimentacao), true);
 END;
 '
